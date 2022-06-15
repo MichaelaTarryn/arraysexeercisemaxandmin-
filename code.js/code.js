294b1cb5abea69b20c879663dd876966cd805371
Michaela <Michaela-13>
2022-06-15 14:23:42 +0200

    first

---
+++ code.js/code.js
@@ -0,0 +1,9 @@
+const numbers = [4, 6, 8, 2, 10,30,98,4,12,5,3];
+//display all elements
+console.log(numbers);
+///display Min Element
+console.log(Math.min(...numbers));
+
+//display max element
+console.log(Math.max(...numbers));
+
